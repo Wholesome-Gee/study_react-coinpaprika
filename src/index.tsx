80d79bbaead9ca4298dd25e23ff3c888c7023cdf
Wholesome-Gee <jiyong0419@naver.com>
2025-05-17 22:29:37 +0900

    gh-pages 작업2
    배포시, wholesome-gee.github.io/study_react-coinpaprika가 Route path="/:coinId"로 인식되어
    BrowserRouter에 basename을 추가함

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query"; // npm i react-query  ( react 18v 이하, 18v 이상에서는 npm i @tanstack/react-query, query key의 값은 대괄호로 묶어주어야한다고 함. )  #5.9 mzd 댓글
+import { QueryClient, QueryClientProvider } from "react-query"; // npm i react-query  
+import { Helmet } from "react-helmet";
+// ( react 18v 이하에서 사용, 18v 이상에서는 npm i @tanstack/react-query, query key의 값은 대괄호로 묶어주어야한다고 함. )  #5.9 mzd 댓글
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
+    <Helmet>
+      <title>Coin View App</title>
+    </Helmet>
     <App />
   </QueryClientProvider>
 );
+
+/*
+react-query를 사용하기위한 사전세팅
+  - const queryClient = new QueryClient();
+  - <QueryClientProvider client={queryClient}>
+*/
